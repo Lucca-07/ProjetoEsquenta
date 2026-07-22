94f133b68fa5725c93dc23da275f453f534e6758
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-07-22 13:56:15 -0300

    style: muda a cor do checkbox selecionado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,7 @@ import CardNumero from "./components/CardNumero/CardNumero";
 export default function App() {
   return (
     <>
-      <CardNumero numero="DDD 11 - 21
-55 11 919339392"></CardNumero>
+      <CardNumero numero="DDD 11 - 21 - 55 11 919339392"></CardNumero>
     </>
   );
 }
