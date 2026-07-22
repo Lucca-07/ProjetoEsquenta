010ef7bd71c911d942b337ee4093fce185514fe8
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-07-22 13:59:34 -0300

    fix: limpa o app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
-import CardNumero from "./components/CardNumero/CardNumero";
-
 export default function App() {
   return (
-    <>
-      <CardNumero numero="DDD 11 - 21 - 55 11 919339392"></CardNumero>
-    </>
+    <div>
+    </div>
   );
 }
