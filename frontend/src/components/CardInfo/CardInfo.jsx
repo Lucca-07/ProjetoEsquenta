6fec83d0ba8b903797feb6723650665eb9efac3b
David Ferreira <dg6491331@gmail.com>
2026-07-22 14:58:20 -0300

    criando-card-de-informacao

--- frontend/src/components/CardInfo/CardInfo.jsx
+++ frontend/src/components/CardInfo/CardInfo.jsx
@@ -3,7 +3,7 @@ import "./CardInfo.css"
 export default function CardInfo ({icon, text}){
     return (
         <div className="card-info">
-            {icon} {text}
+            <div className="icone-info">{icon}</div> {text}
         </div>
     )
 }
