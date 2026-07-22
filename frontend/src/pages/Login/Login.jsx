d1b841fa47b07ad646f1cc1d328e821d903ccef5
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-07-22 15:30:01 -0300

    feat: adiciona pagina de login completa com html e css

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,7 +2,32 @@ import "./Login.css"
 export default function Login() {
     return (
         <div className="login-container">
-            <img className="background-image" src="../../assets/background_login.png" alt="Imagem do Major Vitor Santos" /> 
+            <div className="login-form">
+                <img src="../../../assets/simbolo_pl.svg" alt="Simbolo do PL" />
+                <div className="login-text" style={{ textAlign: 'center'}}>
+                    <p className="montserrat-semibold" style={{ fontSize: '1.75rem' }}>Bem vindo de volta!</p>
+                    <p className="montserrat-regular" style={{ fontSize: '1rem', color: '#666666' }}>Faça login para continuar</p>
+                </div>
+                <div className="input-fields">
+                    <div className="input-container">
+                        <label htmlFor="email" className="montserrat-semibold">Email</label>
+                        <input type="email" id="email" name="email" placeholder="seuemail@gmail.com" />
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="password" className="montserrat-semibold">Senha</label>
+                        <input type="password" id="password" name="password" placeholder="Digite sua senha" />
+                    </div>
+                </div>
+                <div className="login-options">
+                    <div className="remember-me">
+                        <input type="checkbox" id="remember" name="remember" />
+                        <label htmlFor="remember" className="montserrat-regular">Lembrar de mim</label>
+                    </div>
+                    <a href="#" className="montserrat-regular forgot-password">Esqueceu sua senha?</a>
+                </div>
+                <button className="login-button montserrat-semibold">Acessar agora</button>
+
+            </div>
         </div>
     )
 }
