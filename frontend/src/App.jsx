b74d3f79a674a2f343cb6e9725b1632398794655
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-07-22 14:25:54 -0300

    adiciona background na tela de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
+import Login from "./pages/Login/Login.jsx";
 export default function App() {
   return (
     <div>
+      <Login/>
     </div>
   );
 }
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+import "./Login.css"
+export default function Login() {
+    return (
+        <div className="login-container">
+            <img className="background-image" src="../../assets/background_login.png" alt="Imagem do Major Vitor Santos" /> 
+        </div>
+    )
+}
