fe0fedda03c4b5fb1e2efbdca54ecbe4cad73d52
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-07-23 09:50:51 -0300

    feat: adiciona cards de info na pagina

--- frontend/src/pages/Esquenta/Esquenta.jsx
+++ frontend/src/pages/Esquenta/Esquenta.jsx
@@ -1,10 +1,47 @@
 import "./Esquenta.css";
 import Navbar from "../../components/Navbar/Navbar";
+import CardInfo from "../../components/CardInfo/CardInfo";
+import { FaFireAlt, FaMobileAlt, FaCheck } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 export default function Esquenta() {
-  return (
-    <nav>
-        <Navbar/>
-    </nav>
-  );
+    const [valor, setValor] = useState(40);
+
+    // Calcula a porcentagem do preenchimento dinamicamente
+    const preenchimento = {
+        background: `linear-gradient(to right, #4CAF50 0%, #4CAF50 ${valor}%, #ddd ${valor}%, #ddd 100%)`
+    };
+    return (
+        <div className="esquenta-container">
+            <nav className="navbar">
+                <Navbar />
+            </nav>
+            <div className="esquenta-content">
+                <div className="esquenta-mini-cards">
+                    <CardInfo icon={<FaMobileAlt />} text="27 Conectados" ></CardInfo>
+                    <CardInfo icon={<FaFireAlt />} text="12 Esquentando" ></CardInfo>
+                    <CardInfo icon={<FaCheck />} text="27 Concluídos" ></CardInfo >
+                    <CardInfo icon={<AiOutlineClose />} text="13 Não concluídos" ></CardInfo>
+                </div>
+                <div className="esquenta-medium-card">
+                    <div className="esquenta-medium-left-card">
+                        <p className="esquenta-medium-card-title">Progresso</p>
+                        <div className="esquenta-medium-card-content">
+                            <input onChange={(e) => setValor(e.target.value)} style={preenchimento} type="range" min="0" max="100" value={valor} className="esquenta-slider" />
+                        </div>
+                    </div>
+                    <div className="esquenta-medium-right-card">
+                        <p className="esquenta-medium-card-title">Status</p>
+                        <div className="esquenta-medium-card-content">
+                            <p className="esquenta-status-text">A decidir ainda</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="esquenta-bottom-card">
+                    <p className="esquenta-bottom-card-title">Informações</p>
+                </div>
+            </div>
+        </div>
+    );
 }
