b4a12a6065e0a1ced8679decf0e501fc83753c5b
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-07-23 09:15:11 -0300

    feat: cria tela de esquenta e importa navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
+import Esquenta from "./pages/Esquenta/Esquenta";
+
 export default function App() {
   return (
     <div>
+      <Esquenta></Esquenta>
     </div>
   );
 }
---
+++ frontend/src/pages/Esquenta/Esquenta.jsx
@@ -0,0 +1,10 @@
+import "./Esquenta.css";
+import Navbar from "../../components/Navbar/Navbar";
+
+export default function Esquenta() {
+  return (
+    <nav>
+        <Navbar/>
+    </nav>
+  );
+}
